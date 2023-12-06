79441fb4d289b4fd1761a940cf94a3b00ce70905
yoyodes1000 <yoyodes1000@gmail.com>
2023-12-06 20:38:27 +0100

    premier commit

--- src/Molkky.php
+++ src/Molkky.php
@@ -4,10 +4,55 @@ namespace App;
 
 class Molkky
 {
-    public function getScore(array $bowlingSeries)
+    public function getScore(array $bowlingSeries): int
     {
         $result = 0;
 
+
         return $result;
+    
+    }
+
+    public function isOneKeel($array): bool
+    {
+        if (count ($array) === 1){
+            return true;
+        }
+        return false;
+    }
+
+    public function isZeroKeel($array)
+    {
+        if (count ($array) === 0){
+            return true;
+        }
+        return false;
+    }
+
+    public function IsWin(int $value):bool 
+    {
+        if ($value ===50) {
+        return true; 
+        }
+        return false;
+    }
+
+    public function isOver50(int $score): bool
+    {
+        if ($score > 50) {
+            return true; 
+            }
+            return false;
+    }
+
+    public function countPointsByThrow(array $throw): int
+    {
+        if($this->isZeroKeel($throw)){
+            return 0;
+        }
+        if($this->isOneKeel($throw)){
+            return $throw[0];
+        }
+        return count($throw);
     }
 }
--- tests/MolkkyTest.php
+++ tests/MolkkyTest.php
@@ -1,6 +1,46 @@
 <?php
+use App\Molkky;
+use PHPUnit\Framework\TestCase;
 
-class MolkkyTest
+class MolkkyTest extends TestCase
 {
+    public function testIsOneKeel()
+    {
+        $molkky = new Molkky();
+        $this->assertTrue($molkky->isOneKeel([6]));
+        $this->assertFalse($molkky->isOneKeel([6, 8]));
+    }
+    public function testIsZeroKeel()
+    {
+        $molkky = new Molkky();
+        $this->assertTrue($molkky->isZeroKeel([]));
+        $this->assertFalse($molkky->isZeroKeel([6, 8]));
+        $this->assertFalse($molkky->isZeroKeel([6]));
+    }
+    public function testIsWin()
+    {
+        $molkky = new Molkky();
+        $this->assertTrue($molkky->isWin(50));
+        $this->assertFalse($molkky->isWin(49));
+        $this->assertFalse($molkky->isWin(62));
+
+    }
+    public function testIsOver50()
+    {
+        $molkky = new Molkky();
+        $this->assertTrue($molkky->isOver50(60));
+        $this->assertFalse($molkky->isOver50(49));
+        $this->assertFalse($molkky->isOver50(50));
+    }
+    public function testCountPointsByThrow()
+    {
+        $molkky = new Molkky();
+        $this->assertEquals(6, $molkky->countPointsByThrow([6]));
+        $this->assertEquals(2, $molkky->countPointsByThrow([6,3]));
+        $this->assertEquals(0, $molkky->countPointsByThrow([]));
+    }
+
+
+
 
 }
